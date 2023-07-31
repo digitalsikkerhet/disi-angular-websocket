2efa83560b545245ff49fecfcb7077a53f330d17
Per Ove Sunde <pesun@disi.no>
2023-07-31 09:25:49 +0200

    Removed userManagerSettings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,19 +5,7 @@ export const environment = {
   identityserver:
   {
     authority: 'https://localhost:5001',
-    client_id: '22664EEC-7806-4DA8-9361-F790768C1E73',
+    client_id: 'Test_Client_ApiGateway',
     scope: 'openid'
-  },
-  userManagerSettings: 
-  {
-    authority: 'https://localhost:5001',
-    client_id: '22664EEC-7806-4DA8-9361-F790768C1E73',
-    client_secret: 'secret',
-    redirect_uri: 'http://localhost:4200/auth-callback',
-    post_logout_redirect_uri: 'http://localhost:4200/',
-    response_type: "id_token",
-    scope: "openid profile email",
-    filterProtocolClaims: true,
-    loadUserInfo: true
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,25 +1,11 @@
 export const environment = {
   production: false,
   webSocketUrl: 'wss://localhost:44323',  
-  secureRoutes:  [ 'wss://localhost:44323', 'wss://websocket.disi.no' ],
+  secureRoutes:  [ 'wss://localhost:44323' ],
   identityserver:
   {
     authority: 'https://localhost:5001',
     client_id: 'Test_Client_ApiGateway',
-    client_secret: 'secret',
     scope: 'openid'
-  },
-  userManagerSettings: 
-  {
-    authority: 'https://localhost:5001',
-    client_id: '22664EEC-7806-4DA8-9361-F790768C1E73',
-    client_secret: 'secret',
-    redirect_uri: 'http://localhost:4200/auth-callback',
-    post_logout_redirect_uri: 'http://localhost:4200/',
-    response_type: "id_token",
-    scope: "api",
-    filterProtocolClaims: true,
-    loadUserInfo: true,
-    grant_types_supported: ['client_credentials', 'password']
   } 
 };
